6f501baf6d8836eff1ad62f0f6e70021506cf521
anthonybahls-ai <anthony.bahls@escola.pr.gov.br>
2026-04-07 08:39:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Seleção de elementos
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links li');
 
+// Menu Hambúrguer Mobile
+hamburger.addEventListener('click', () => {
+    // Abre/Fecha Nav
+    navLinks.classList
